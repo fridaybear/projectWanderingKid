62d5c44c8175b0ad7b76aa8fddd1fa7bd9514b54
fridaybear <68036166+fridaybear@users.noreply.github.com>
2025-10-18 16:43:06 +0100

    fixes

---
+++ Assets/resetEnemyPosition.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class resetEnemyPosition : MonoBehaviour
+{
+
+    public Camera mainCam;
+    public Vector2 defaultPosition;
+    
+    bool IsInView(Camera cam, Vector3 worldPosition)
+    {
+        Vector3 viewportPos = cam.WorldToViewportPoint(worldPosition);
+
+        return viewportPos.z > 0 && 
+            viewportPos.x > 0 && viewportPos.x < 1 && 
+            viewportPos.y > 0 && viewportPos.y < 1;   
+    }
+
+    void Start()
+    {
+        defaultPosition = transform.position;
+    }
+
+    void Update()
+    {
+
+        if (!IsInView(mainCam, transform.position))
+        {
+            transform.position = defaultPosition;
+        }
+        
+    }
+}
