3673f0961719f204f38e0bd21745f7c03c35c967
Archie Houghton <archiejhoughton@icloud.com>
2025-10-19 14:40:15 +0100

    Added animations to butterfly guide

--- Assets/scripts/GuideMovement.cs
+++ Assets/scripts/GuideMovement.cs
@@ -1,18 +1,44 @@
 using UnityEngine;
-using UnityEngine.InputSystem.LowLevel;
+using System.Collections;
 
-public class GuideMovement: MonoBehaviour
+public class GuideMovement : MonoBehaviour
 {
-    public int speed = 4;
-    
-public GameObject cave;
+    public int speed = 3;
+
+    public Sprite[] sprites; // Drag all sliced frames here (from your sprite sheet)
+    private SpriteRenderer sr;
+
+    public int animationCycle = 0;
+
+    public GameObject cave;
+
     void Start()
     {
-         
+        sr = GetComponent<SpriteRenderer>();
+        StartCoroutine(DoSomethingWithDelay());
     }
+
     void Update()
     {
-       transform.position = Vector3.MoveTowards(transform.position, cave.transform.position, speed * Time.deltaTime);   
+        transform.position = Vector3.MoveTowards(transform.position, cave.transform.position, speed * Time.deltaTime);
+    }
+
+    private IEnumerator DoSomethingWithDelay()
+    {
+        while (true)
+        {
+            if (animationCycle == 0)
+            {
+                sr.sprite = sprites[0];
+                animationCycle = 1;
+            }
+            else
+            {
+                sr.sprite = sprites[1];
+                animationCycle = 0;
+            }
+
+            yield return new WaitForSeconds(0.2f); // Wait after each frame swap
+        }
     }
 }
-
